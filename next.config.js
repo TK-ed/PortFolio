4ed729761b201dfbd5eb0f1c19ce8b88446add02
TK-ed <tharunstark11@gmail.com>
2024-04-24 01:22:21 +0530

    Resolved deployment error by fixing module import issues with core-js dependencies

--- next.config.js
+++ next.config.js
@@ -7,12 +7,32 @@ module.exports = {
 			'imgur.com',
 		],
 	},
-	webpack(config) {
+	webpack(config, {isServer}) {
+		config.optimization.minimize = false;
+		// Custom rule for .graphql files
 		config.module.rules.push({
 			test: /\.graphql$/,
 			exclude: /node_modules/,
 			use: [{loader: 'graphql-tag/loader'}],
 		});
+
+		// Modify TerserPlugin settings
+		if (!isServer) {
+			config.optimization.minimizer = config.optimization.minimizer.map(
+				plugin => {
+					if (plugin.constructor.name === 'TerserPlugin') {
+						plugin.options.terserOptions = {
+							...plugin.options.terserOptions,
+							// Add or modify Terser options as needed:
+							keepClassnames: true,
+							keepFnames: true,
+						};
+					}
+
+					return plugin;
+				},
+			);
+		}
 
 		return config;
 	},
