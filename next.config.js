577b2fbbc0630341cf810ce044b492610476ab06
TK-ed <tharunstark11@gmail.com>
2024-04-24 00:34:19 +0530

    Configured ESLint for consistent code styling and quality checks

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  images: {
-    unoptimized: true,
-    domains: [
-      'res.cloudinary.com',
-      'avatars.githubusercontent.com',
-      'imgur.com',
-    ],
-  }
+	images: {
+		unoptimized: true,
+		domains: [
+			'res.cloudinary.com',
+			'avatars.githubusercontent.com',
+			'imgur.com',
+		],
+	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.graphql$/,
+			exclude: /node_modules/,
+			use: [{loader: 'graphql-tag/loader'}],
+		});
+
+		return config;
+	},
 };
